24a29ee49008c205de80d8d78b27155d279937db
lonfee88 <lonfee88@gmail.com>
2014-06-24 15:21:04 +0800

    非阻塞

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,20 @@
 var exec = require("child_process").exec;
 
-function start() {
-	console.log("Request handler 'start' was called.");
-	var content = "empty";
+function start(response) {
+  console.log("Request handler 'start' was called.");
 
-	exec("ls -lah", function(error, stdout, stderr){
-		content = stdout;
-	});
-
-	return content;
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-
-function upload() {
-	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+function upload(response) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
